3644231b1bc9f5e2a110d414044e658181c08ac4
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-08-23 00:05:21 -0500

    Add DTO and entity classes for Persona and PersomaCustum

---
+++ src/main/java/com/isai/model_mapper/dtos/PersomaCustumDto.java
@@ -0,0 +1,10 @@
+package com.isai.model_mapper.dtos;
+
+public class PersomaCustumDto {
+    private Long idDto;
+    private String nombreDto;
+    private String apellidoDto;
+    private String emailDto;
+    private byte ageDto;
+    private Character genderDto;
+}
---
+++ src/main/java/com/isai/model_mapper/dtos/PersonaDefaultDto.java
@@ -0,0 +1,72 @@
+package com.isai.model_mapper.dtos;
+
+public class PersonaDefaultDto {
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private byte age;
+    private Character gender;
+
+    PersonaDefaultDto() {
+
+    }
+
+    public PersonaDefaultDto(Long id, String nombre, String apellido, String email, byte age, Character gender) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public byte getAge() {
+        return age;
+    }
+
+    public void setAge(byte age) {
+        this.age = age;
+    }
+
+    public Character getGender() {
+        return gender;
+    }
+
+    public void setGender(Character gender) {
+        this.gender = gender;
+    }
+
+}
